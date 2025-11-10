c3dfb7cb6614d48d67c47803dbebd40bb7226fea
Diarko <diana.kovtun.ua@gmail.com>
2025-11-10 03:01:18 +0200

    deleted unnecessary files and renamed indeed file

--- test_playwright.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import sync_playwright
-with sync_playwright() as p: 
-    browser = p.chromium.launch(headless = True)
-    context = browser.new_context(ignore_https_errors=True)
-    page = browser.new_page()
-    page.goto("https://scrapeme.live/shop/")
-    print(page.title())
-    browser.close()
