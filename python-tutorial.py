9ba4a032dac4ba799a5166f1443c0908f3cf0326
Diarko <diana.kovtun.ua@gmail.com>
2025-11-10 02:52:02 +0200

    Readme end toml

--- python-tutorial.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-url = "https://stackoverflow.com/questions/5338979/scraping-a-web-page-with-java-script-in-python"
-response = requests.get(url)
-soup = BeautifulSoup (response.text, "html.parser")
-first_paragraph = soup.find("p")
-print (first)
